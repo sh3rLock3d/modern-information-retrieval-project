05496587d3201acb57c33c252b8c93303fbb92cc
sh3rLock3d <alipourghsaemi>
2020-11-13 21:53:44 +0330

    SearchAnsRetrival.py added

---
+++ SearchAnsRetrival.py
@@ -0,0 +1,9 @@
+class SearchAnsRetrival:
+    def __init__(self, query):
+        self.query = query
+        self.run()
+
+
+    def run(self):
+        # todo create user interface
+        pass
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from SearchAnsRetrival import SearchAnsRetrival
 from compressing import Compress
 from indexing import Indexing
 from query_check import query_check
@@ -7,7 +8,7 @@ def main():
     my_index = Indexing()
     my_compress = Compress(my_index)
     my_query_check = query_check(my_compress)
-    # jostoju va bazyabi asnad
+    SearchAnsRetrival(my_query_check)
 
 
 if __name__ == '__main__':
