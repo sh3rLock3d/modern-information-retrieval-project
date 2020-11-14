bb92e38b5227bd42c9d0205cb803500feb887819
mohammad-fattah <m.p.fattahpour@gmail.com>
2020-11-15 00:42:49 +0330

    little change

--- compressing.py
+++ compressing.py
@@ -102,10 +102,10 @@ class CompressUtils:
             postings = [indexing.ted_talk_ii.dictionary.get(key)[1][i].doc_id for i in range(len(indexing.ted_talk_ii.dictionary.get(key)[1]))]
             positions = [indexing.ted_talk_ii.dictionary.get(key)[1][i].positions for i in range(len(indexing.ted_talk_ii.dictionary.get(key)[1]))]
             for position in positions:
-                position_list.append(int(G.gamma_encoder(position),2).to_bytes(math.ceil(len(G.gamma_encoder(position)) / 8,sys.byteorder)))
-            gamma_list.append(int(G.gamma_encoder(postings),2).to_bytes(math.ceil(len(G.gamma_encoder(postings)) / 8,sys.byteorder)))
+                position_list.append(int(G.gamma_encoder(position),2).to_bytes(math.ceil(len(G.gamma_encoder(position)) / 8),sys.byteorder))
+            gamma_list.append(int(G.gamma_encoder(postings),2).to_bytes(math.ceil(len(G.gamma_encoder(postings)) / 8),sys.byteorder))
         gamma_file_ii = open('gamma_code_ii', 'ab')
-        pickle.dump(tuple(position_list,gamma_list),gamma_file_ii)
+        pickle.dump((position_list,gamma_list),gamma_file_ii)
         gamma_file_ii.close()
         gamma_file_kg = open('gamma_code_kg', 'ab')
         pickle.dump(indexing.ted_talk_kg.dictionary,gamma_file_kg)
