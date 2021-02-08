4cbfbaf706317f2a3f2ac259c49061ffa827569a
sh3rlock3d <alipourghsaemi>
2021-02-08 08:59:21 +0330

    add weight to pagerank

--- phase3/pagerank.py
+++ phase3/pagerank.py
@@ -5,20 +5,21 @@ from scipy import sparse
 
 edges = list()
 nodes_name = dict()
-
+edge_weight = dict()
 
 def add_link(from_node, to_node):
     for name in from_node, to_node:
         if name not in nodes_name.keys():
             nodes_name[name] = len(nodes_name)
     f, t = nodes_name[from_node], nodes_name[to_node]
+    edge_weight[(f,t)] = edge_weight.get((f,t), 0) + 1
     edges.append([f, t])
 
 
 def find_pagerank(alpha=0.85):
     n = len(nodes_name)
     A = np.array(edges)
-    weights = [1 for _ in range(len(edges))]
+    weights = [edge_weight[(A[i][0], A[i][1])] for i in range(len(edges))]
     G = sparse.csr_matrix((weights, (A[:, 0], A[:, 1])), shape=(n, n))
     pr = pagerank(G, p=alpha)
     return [(node, pr[node_num]) for node, node_num in nodes_name.items()]
